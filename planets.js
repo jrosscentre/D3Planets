d5b5a539df3d663f9b006d4ccd984071e4db25d4
Jerry <j.ross@centre.edu>
2019-10-26 15:37:08 -0400

    done

--- planets.js
+++ planets.js
@@ -5,20 +5,102 @@ d3.select("#A2").selectAll("span").data(Names).enter().append("span").text(funct
 
 
 planetpromise = d3.json ("planets.json");
-planetpromise.then(function(plntdata)
-                  {
-    console.log("works",plntdata)
+planetpromise.then(
     
-})
-
-var plntimg = funtion(plntdata)
+function(plntdata)
 {
-    var imgs = d3.select("#B3").data(plntdata).append("ul")
-    var plntlist {plntlist.selectAll("li")
-    .data(plntdata)
-    .append("li")
-    .enter()
-        items.append("img").attr("src",function(d){return d.picture})
+    console.log("works",plntdata)
+    imgslist(plntdata); 
+    imgsol(plntdata);
+    maketable(plntdata);
+    tablewnames(plntdata);
+    fulltable(plntdata)
+},
+function(err)
+{
+    console.log("doesn't",err)
 }
+    )
+
+
+    var imgslist = function (imgs)
+    {
+        d3.select("#B3")
+        .selectAll("img")
+        .data(imgs)
+        .enter()
+        .append("img")
+        .attr("src",function(planet)
+              {return planet.img;})
+    }
+     
+    
+    var imgsol = function(imgs)
+    {
+         d3.select("#B4")
+        .selectAll("ol")
+        .data(imgs)
+        .enter()
+        .append("li")
+        .text(function(planet)
+              {return planet.name;})
+    }
+    
+    var maketable = function(imgs)
+    {
+        console.log("works")
+        d3.select("#C1")
+        .append("table")
+        .selectAll("tr")
+        .data(imgs)
+        .enter()
+        .append("tr")
+    }
+    
+    var tablewnames = function(imgs)
+    {
+        console.log("works")
+         d3.select("#C2")
+        .append("table")
+        .selectAll("tr")
+        .data(imgs)
+        .enter()
+        .append("tr")
+        .text(function(d)
+               {return d.name;})
+    }
+    
+    var fulltable = function(imgs)
+    {
+      console.log("works")
+         d3.select("#C3")
+        .append("table")
+        .selectAll("tr")
+        .data(imgs)
+        .enter()
+        .selectAll("tr")
+        .append("tr")
+        .text(function(d)
+              {return d.name;})
+        .append("td")
+        .text(function(d)
+             {return d.distance;})
+        .append("td")
+        .text(function(d)
+             {return d.radius;})
+        .append("td")
+        .text(function(d)
+             {return d.density;})
+        .append("td")
+        .text(function(d)
+             {return d.moons})
+        .append("tr")
+        .append("td")
+        .attr("src", function (d)
+             {return d.img;})
+    }
+    
+    
+
 
  
